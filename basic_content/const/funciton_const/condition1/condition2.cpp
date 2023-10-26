dfbf5e9259d6795b46d57e83bdac3c7d6c3c036c
wang-hui21 <1024099045@qq.com>
2023-10-26 21:26:40 +0800

    代码修改

--- basic_content/const/funciton_const/condition1/condition2.cpp
+++ basic_content/const/funciton_const/condition1/condition2.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main() {
   const int p = 10;
-  const void *vp = &p;
-  void *vp = &p; // error
+  const int *vp = &p;      //const void 指针是不允许的，因为 void 类型本身是无类型的，它没有大小和值，不能用于指向普通变量。
+//  void *vp = &p; // error
+  cout<<(*vp)<<endl;
 }
